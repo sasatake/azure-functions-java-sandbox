4944d1f01ff11b9ba854a2f553ae0cdef67d391d
sasatake <t06418as@gmail.com>
2022-06-20 07:19:06 +0900

    fix queue name

---
+++ src/main/java/net/azurewebsites/functions/java/sandbox/Constants.java
@@ -0,0 +1,7 @@
+package net.azurewebsites.functions.java.sandbox;
+
+public class Constants {
+
+  public static final String EBOOK_SUMMARY_QUEUE_NAME = "get-ebook-summary-jobs";
+  public static final String EBOOK_DETAIL_QUEUE_NAME = "get-ebook-detail-jobs";
+}
